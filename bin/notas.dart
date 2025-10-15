f41bcaf4a948274a06a964b151f9c0a481e855dd
Felipe Vargas <felipelwvargas@gmail.com>
2025-10-15 15:03:37 -0300

    Função adicionar Lista

--- bin/notas.dart
+++ bin/notas.dart
@@ -1,5 +1,32 @@
-import 'package:notas/notas.dart' as notas;
+import 'dart:io';
 
-void main(List<String> arguments) {
-  print('Hello world: ${notas.calculate()}!');
+void main() {
+  List<String> notas = <String>[];
+  getComando();
+  adicionaNota(notas);
+}
+
+String getComando(){
+  print ("Digite um comando: 1 - Adicionar nota, 2 - listar notas, 3 - Sair");
+  List<String> comandos = <String>["1","2","3"];
+  String? entrada ="";
+
+  entrada = stdin.readLineSync();
+
+  if (entrada == null || !comandos.contains(entrada)){
+    print("Comando invalido!");
+    getComando();
+  }
+  return entrada!;
+}
+List<String> adicionaNota(List<String> notas){
+  print("Escreva uma nota!");
+  String? nota = "";
+  nota = stdin.readLineSync();
+  if(nota == null || nota.isEmpty){
+    print("Não é possível adicionar uma nota vazia!");
+    adicionaNota(notas);
+  }
+  notas.add(nota!);
+  return notas;
 }
